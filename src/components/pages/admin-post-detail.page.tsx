3fd9d5581fa1950ae5438f6fc6232e048defac8c
douqin <dagxuanlam@gmail.com>
2025-04-09 16:48:16 +0700

    fix: error ui in register page

--- src/components/pages/admin-post-detail.page.tsx
+++ src/components/pages/admin-post-detail.page.tsx
@@ -190,7 +190,7 @@ export default function PendingPostDetailPage() {
                                 Your browser does not support the video tag.
                               </video>
                             ) : (
-                              <></>
+                              <> </>
                             )
                           )}
                         </Image.PreviewGroup>
